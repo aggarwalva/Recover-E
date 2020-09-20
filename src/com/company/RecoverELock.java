e607e9ae82f5e1230bed40ed5d0f45ea8bdc3d9d
aggarwalva <varunneilaggarwal@gmail.com>
2020-09-20 04:46:08 -0400

    Added client side file lock

--- src/com/company/RecoverELock.java
+++ src/com/company/RecoverELock.java
@@ -1,6 +1,18 @@
 package com.company;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import net.lingala.zip4j.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
@@ -11,40 +23,196 @@ import net.lingala.zip4j.model.enums.CompressionMethod;
 import net.lingala.zip4j.model.enums.EncryptionMethod;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class RecoverELock extends Application {
+    private ArrayList<File> files;
+    private int fileNum;
+    private int fileIterator;
 
     public static void main(String[] args) {
         //System.out.println(generatePassword("HelloWorld", new byte[16]));
-        createZip();
+        //createZip();
+        launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        stage.setTitle("Recover-E Lock");
+        GridPane root = new GridPane();
+        Text group = new Text("Recover-E Lock");
+        group.setScaleX(2);
+        group.setScaleY(2);
+        group.setScaleZ(2);
 
+        Button start = new Button("Start");
+        start.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                fileSelection(stage);
+            }
+        });
+        root.setAlignment(Pos.TOP_CENTER);
+        root.setVgap(10);
+
+        start.setAlignment(Pos.CENTER);
+        root.add(group, 0, 15);
+        root.add(start, 0, 20);
+        GridPane.setHalignment(start, HPos.CENTER);
+        stage.setScene(new Scene(root, 640, 480));
+
+        stage.show();
     }
 
-    private static void createZip() {
-        ZipFile testzip = new ZipFile("testzipfile.zip");
-        System.out.println("Made zip");
+    private void fileSelection(Stage stage) {
+        Stage popupStage = new Stage();
+        popupStage.setTitle("Enter number of files");
+        TextField inPath = new TextField("Enter number of files here");
+        Button next = new Button("Next");
+
+        next.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                try {
+                    fileNum = Integer.parseInt(inPath.getText());
+                    inPath.setPromptText("Enter file path here");
+                    popupStage.setTitle("Enter file path");
+                    fileIterator = 0;
+                    files = new ArrayList<>();
+                    next.setOnAction(new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle(ActionEvent actionEvent) {
+                            if(fileIterator < fileNum) {
+                                try {
+                                    File temp = new File(inPath.getText());
+                                    if(temp.exists()) {
+                                        files.add(temp);
+                                    } else {
+                                        throw new FileNotFoundException();
+                                    }
+                                    fileIterator++;
+                                } catch (Exception e) {
+                                    Popup("Error", "Please enter a valid file path");
+                                    e.printStackTrace();
+                                }
+                                if(fileIterator >= fileNum) {
+                                    popupStage.close();
+                                    closingScreen(stage);
+                                }
+                            } else {
+                                popupStage.close();
+                                closingScreen(stage);
+                            }
+                        }
+                    });
+                } catch(Exception e) {
+                    Popup("Error", "Please input a valid number");
+                }
+            }
+        });
+
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.TOP_CENTER);
+        root.setHgap(25);
+        root.setVgap(10);
+        root.setPadding(new Insets(25,25,25,25));
+        root.add(inPath, 0,0);
+        root.add(next,0,1);
+        popupStage.setScene(new Scene(root, 500,200));
+        popupStage.show();
+    }
+
+    private void closingScreen(Stage stage) {
+        ZipFile lockzip = new ZipFile("Lockdown.zip");
+
+        long key = System.currentTimeMillis();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        String time = dtf.format(now);
+
+        File timestamp = new File("Timestamp.txt");
+        try {
+            timestamp.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            FileWriter writer = new FileWriter(timestamp);
+            writer.write(time);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        files.add(timestamp);
+
+        File keyFile = new File("key.txt");
+        try {
+            keyFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            FileWriter writer = new FileWriter(keyFile);
+            writer.write(Long.toString(key));
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         ZipParameters parameters = new ZipParameters();
         parameters.setCompressionMethod(CompressionMethod.DEFLATE);
         parameters.setCompressionLevel(CompressionLevel.NORMAL);
         parameters.setEncryptFiles(true);
         parameters.setEncryptionMethod(EncryptionMethod.ZIP_STANDARD);
-        System.out.println("Set params");
+
+        String password = generatePassword(Long.toString(key), new byte[16]);
+        lockzip.setPassword(password.toCharArray());
+        try {
+            lockzip.addFiles(files, parameters);
+        } catch (ZipException e) {
+            e.printStackTrace();
+        }
+
+        GridPane root = new GridPane();
+        Text group = new Text("Recover-E Lock");
+        group.setScaleX(2);
+        group.setScaleY(2);
+        group.setScaleZ(2);
+
+        Text text = new Text("Lockdown finished");
+
+        root.setAlignment(Pos.TOP_CENTER);
+        root.setVgap(10);
+
+        root.add(group, 0, 15);
+        root.add(text, 0, 20);
+        GridPane.setHalignment(text, HPos.CENTER);
+        stage.setScene(new Scene(root, 640, 480));
+
+        stage.show();
+    }
+
+    private static void createZip() {
+        ZipFile testzip = new ZipFile("testzipfile.zip");
+
+        ZipParameters parameters = new ZipParameters();
+        parameters.setCompressionMethod(CompressionMethod.DEFLATE);
+        parameters.setCompressionLevel(CompressionLevel.NORMAL);
+        parameters.setEncryptFiles(true);
+        parameters.setEncryptionMethod(EncryptionMethod.ZIP_STANDARD);
         testzip.setPassword("HelloWorld".toCharArray());
         try {
             testzip.addFile(new File("C:\\Users\\Varun\\Desktop\\zippy.txt"), parameters);
         } catch (ZipException e) {
             e.printStackTrace();
         }
-        System.out.println("Added file");
-
-        System.out.println("Done");
     }
 
     private static String generatePassword(String key, byte[] salt)
@@ -66,5 +234,16 @@ public class RecoverELock extends Application {
             e.printStackTrace();
         }
         return generatedPassword;
+    }
+
+    public void Popup(String title, String message) {
+        Stage stage = new Stage();
+        stage.setTitle(title);
+        Text text = new Text(message);
+        GridPane pane = new GridPane();
+        pane.setAlignment(Pos.CENTER);
+        pane.add(text, 0, 0);
+        stage.setScene(new Scene(pane, 300, 150));
+        stage.show();
     }
 }
